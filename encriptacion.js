47ad8922e456a4646133a070e419a2958ece3c27
Richard <peralesrichard25@gmail.com>
2023-01-06 22:30:20 -0500

    Creando la funcion de desencriptar

--- encriptacion.js
+++ encriptacion.js
@@ -15,3 +15,17 @@ const Encriptar=()=>{
         document.getElementById('resultadoEncriptar').value = textoEncriptado;
     }
 }
+
+const Desencriptar=()=>{
+    var texto = document.getElementById('textoEncriptar').value;
+    if(texto == ""){
+        resultado.style.display = 'none';
+        error.style.display = '';
+    }else{
+        error.style.display = 'none';
+        resultado.style.display = '';
+        var textoEncriptado = texto.replace(/enter/gi,"e").replace(/imes/gi,"i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+        document.getElementById('textoEncriptar').value = "";
+        document.getElementById('resultadoEncriptar').value = textoEncriptado;
+    }
+}
